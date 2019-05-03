a822e8de5e334cb1a9699aa63b1f84c286ced4b7
Nathalia Andrade <nathaliandrad@gmail.com>
2019-05-03 20:22:03 +1000

    final screens

---
+++ menu_scene/game_project_screen/menu_screen/Game Project/Entity.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SDL.h>
+#include "Vector.h"
+
+class Entity
+{
+protected:
+	SDL_Renderer* renderer;
+	
+public:
+	Vector position; // x,y representing where it is on the level 
+	Vector velocity; // how fast and which direction the thing is moving
+	Vector acceleration; //how much to increase/decrease velocity
+
+	Entity();
+	~Entity();
+
+	void setRenderer(SDL_Renderer* renderer);
+	void setXY(float x, float y);
+	float getX();
+	float getY();
+
+	virtual void update(float dt);
+	virtual void updateMovement(float dt);
+	virtual void draw();
+
+};
+
---
+++ menu_scene/game_project_screen/menu_screen/Game Project/Wall.cpp
@@ -0,0 +1,23 @@
+#include "Wall.h"
+
+
+Wall::Wall()
+{
+}
+
+
+Wall::~Wall()
+{
+}
+
+
+void Wall::setWH(float w, float h){
+	this->w = w;
+	this->h = h;
+}
+
+void Wall::draw(){
+	SDL_Rect wallRect = { position.x, position.y, w, h };
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderFillRect(renderer, &wallRect);
+}
